488972708d06f83bf0d18dee3a78718d9e4355d6
Olav Valle <olavval@stud.ntnu.no>
2019-12-21 18:06:15 +0100

    adds set methods to Item

--- src/Item.java
+++ src/Item.java
@@ -1,3 +1,7 @@
+/**
+ * Class representing items. Holds information about an item, including
+ * item name, description and weight.
+ */
 public class Item {
 
     private String name;
@@ -7,22 +11,35 @@ public class Item {
     //TODO implement different item types; weapon, consumable, etc...
 
     /**
-     * Item object constructor.
+     * Item object constructor. Only requires
      * @param name name of item.
      */
     public Item(String name)
     {
         this.name = name;
-
+        this.weight = 0;
+        this.description = "";
     }
 
+    /**
+     * Set item description string.
+     * @param description string describing item.
+     */
     public void setDescription(String description)
     {
         if((description != null) && !(description.isEmpty()))
         {
-
+            this.description = description;
         }
     }
 
+    /**
+     * Set item weight.
+     * @param weight int value of item weight in metric grams.
+     */
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
 
 }
